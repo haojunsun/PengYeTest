afc5c785012cfe646c02aae57d0c56a4f669d24e
sunhaojun <coolboss1@sina.com>
2017-05-04 14:28:20 +0800

    ef 搭建

---
+++ PengYe.Project.Repository/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PengYe.Project.Infrastructure;
+
+namespace PengYe.Project.Repository
+{
+    public class MyDbContext:DbContext, IDependencyPerRequest
+    {
+        public MyDbContext(): base("DefaultConnection")//数据连接
+        {
+
+        }
+    }
+}
