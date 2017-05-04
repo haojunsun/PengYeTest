99afbe4c8034164fb087e2654bdb9d67b7842fac
sunhaojun <coolboss1@sina.com>
2017-05-04 15:23:27 +0800

    添加log

---
+++ PengYe.Project.Log/LogService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using PengYe.Project.Infrastructure;
+
+namespace PengYe.Project.Log
+{
+    public interface ILogService : IDependency
+    {
+        void Debug(string msg);
+        void Error(string msg);
+        void Info(string msg);
+
+        void Warn(string msg);
+    }
+
+    public class LogService : ILogService
+    {
+        private readonly ILog _log = LogManager.GetLogger("AppLog.Logging");
+        public void Debug(string msg)
+        {
+            _log.Debug(msg);
+        }
+
+        public void Error(string msg)
+        {
+            _log.Error(msg);
+        }
+
+        public void Info(string msg)
+        {
+            _log.Info(msg);
+        }
+
+        public void Warn(string msg)
+        {
+            _log.Warn(msg);
+        }
+    }
+}
